78f0c83e5ec9cd2853d50eba208670ccbfe0f2d6
waakye <dailyomnivore@gmail.com>
2017-04-28 03:05:18 +0800

    Check network connectivity status

--- app/src/main/java/com/waakye/gbl/MainActivity.java
+++ app/src/main/java/com/waakye/gbl/MainActivity.java
@@ -1,7 +1,10 @@
 package com.waakye.gbl;
 
 import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -46,16 +49,33 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         // So the adapter on the {@link ListView} so the list can be populated in the user interface
         listView.setAdapter(mAdapter);
 
-        // Get a reference to the LoaderManager, in order to interact with loaders
-        LoaderManager loaderManager = getLoaderManager();
 
-        // Initialize the loader.  Pass in the int ID constant defined above and pass in null for
-        // the bundle
-        loaderManager.initLoader(BOOKLISTING_LOADER_ID, null, this);
 
-//        // Start the AsyncTask to fetch the booklistings data
-//        BookListingAsyncTask task = new BookListingAsyncTask();
-//        task.execute(GOOGLE_BOOKS_QUERY);
+        // Get a reference to the ConnectivityManager to check the state of network connectivity
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Get details on the currently active default data network
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+
+        // If there is a network connection, fetch data
+        if (networkInfo != null && networkInfo.isConnected()) {
+            // Get a reference to the LoaderManager, in order to interact with loaders
+            LoaderManager loaderManager = getLoaderManager();
+
+            // Initialize the loader.  Pass in the int ID constant defined above and pass in null for
+            // the bundle
+            loaderManager.initLoader(BOOKLISTING_LOADER_ID, null, this);
+        } else {
+            // Otherwise, display error
+            // First, hide the loading indicator so error message will be visible
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+
+            // Update empty state with no connection error message
+            mEmptyStateTextView.setText(R.string.no_internet_connection);
+        }
+
     }
 
     @Override
