f441488d0542ac38cfad232eb21653ca0fc9f7db
waakye <dailyomnivore@gmail.com>
2017-04-28 01:52:06 +0800

    Add AsyncTask

--- app/src/main/java/com/waakye/gbl/MainActivity.java
+++ app/src/main/java/com/waakye/gbl/MainActivity.java
@@ -1,27 +1,62 @@
 package com.waakye.gbl;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOG_TAG = MainActivity.class.getName();
+
+    private static final String GOOGLE_BOOKS_QUERY
+    = "https://www.googleapis.com/books/v1/volumes?q=hawthorne&key=AIzaSyA49wUdKh_qlFv5FiexsdYdLyv8rNEDiko";
+
+    /** Adapter for the list of booklistings */
+    private BookListingAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
-        // Create a fake list of books
-        ArrayList<BookListing> bookListings = QueryUtils.extractBookListings();
-
-        // Create an {@link BookListingAdapter} whose data source is a list of {@link BookListing}s.
-        // The adapter knows how to create list item views for each item in the list
-        BookListingAdapter bookListingAdapter = new BookListingAdapter(this, bookListings);
-
-        // Get a reference to the ListView and attach the adapter to the listView
+        // Find a reference to the {@link ListView} in the layout
         ListView listView = (ListView)findViewById(R.id.listview_booklisting);
-        listView.setAdapter(bookListingAdapter);
+
+        // Create a new adapter that takes an empty list of booklistings as inputs
+        mAdapter = new BookListingAdapter(this, new ArrayList<BookListing>());
+
+        // So the adapter on the {@link ListView} so the list can be populated in the user interface
+        listView.setAdapter(mAdapter);
+
+        // Start the AsyncTask to fetch the booklistings data
+        BookListingAsyncTask task = new BookListingAsyncTask();
+        task.execute(GOOGLE_BOOKS_QUERY);
+    }
+
+    private class BookListingAsyncTask extends AsyncTask<String, Void, List<BookListing>> {
+
+        @Override
+        protected List<BookListing> doInBackground(String... urls) {
+            // Don't perform the request if there is no URLs, or the first URL is null
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            List<BookListing> result = QueryUtils.fetchBookListingData(GOOGLE_BOOKS_QUERY);
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(List<BookListing> data) {
+            mAdapter.clear();
+
+            if (data != null && !data.isEmpty()) {
+                mAdapter.addAll(data);
+            }
+        }
     }
 }
